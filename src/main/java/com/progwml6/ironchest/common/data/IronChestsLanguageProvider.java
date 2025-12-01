62e1425a370c25931813c14c29a7f2d489c0cc18
Alexander Behrhof <alexanderbehrhof@toontownrewritten.com>
2025-11-30 19:33:17 -0500

    Add support for translatable DirtChest9000 Book Titles

--- src/main/java/com/progwml6/ironchest/common/data/IronChestsLanguageProvider.java
+++ src/main/java/com/progwml6/ironchest/common/data/IronChestsLanguageProvider.java
@@ -55,6 +55,8 @@ public class IronChestsLanguageProvider extends LanguageProvider {
     this.add("ironchest.container.crystal_chest", "Crystal Chest");
     this.add("ironchest.container.obsidian_chest", "Obsidian Chest");
     this.add("ironchest.container.dirt_chest", "DirtChest 9000!");
+
+    this.add("ironchest.book.dirtchest9000.title", "How to use your DirtChest 9000!");
   }
 
   public void addBookAndContents(String bookKey, String bookTitle, String... pages) {
